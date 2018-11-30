a21af3123dff738f263951da3222bf1c2114d989
hustlion-dev <hustlion_dev@163.com>
2018-11-30 15:19:00 +0800

    singleton ok

---
+++ spec/singleton_spec.lua
@@ -0,0 +1,12 @@
+describe("Singleton", function()
+  local singleton = nil;
+  setup(function()
+    require("../singleton");
+  end)
+  it("should make sure return the same instance", function() 
+    local s1 = Singleton:Instance();
+    local s2 = Singleton:Instance();
+    assert.is.equal(s1, s2);
+  end)
+
+end)
