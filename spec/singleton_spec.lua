6a6aa6a6ae591e8e017d8e9eccc52e1cacb02f0d
hustlion-dev <hustlion_dev@163.com>
2018-11-30 15:20:01 +0800

    fix grammar

--- spec/singleton_spec.lua
+++ spec/singleton_spec.lua
@@ -3,7 +3,7 @@ describe("Singleton", function()
   setup(function()
     require("../singleton");
   end)
-  it("should make sure return the same instance", function() 
+  it("should return the same instance", function() 
     local s1 = Singleton:Instance();
     local s2 = Singleton:Instance();
     assert.is.equal(s1, s2);
